98b9ca8c0e6bcd8617b02fd2740153447c6e6677
Rob <Zhouenkissinger@gmail.com>
2018-12-12 14:52:12 -0500

    It's at a finishing point.

--- requestsproject.py
+++ requestsproject.py
@@ -2,10 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-# target CIK
-target = '0001166559'
 
-
+#function to find the page containing the 13F filings.
 def find_page(target):
     # go right to a search that just shows 13F filings 
     search_return = requests.get('https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=' + target +'&type=13F&dateb=&owner=exclude&output=xml&count=100')
@@ -14,15 +12,21 @@ def find_page(target):
     soup = BeautifulSoup(search_return.content, "html5lib")
     results = soup.find_all('filing')
     
-    # find the first result
-    result = results[0]
-    #get the link
-    result_link = result.find('filinghref')
-    page_link = result_link.text
+    if len(results) == 0:
+        print('No luck finding a 13F report for ' + target +'.  Continuing on to next CIK.')
 
-    #return the link
-    return page_link
+    else: 
+        # find the first result
+        result = results[0]
+        #get the link
+        result_link = result.find('filinghref')
+        page_link = result_link.text
 
+        #return the link
+        find_xml(page_link)
+
+
+#function to find the report
 def find_xml(url):
     # go to the filings page
     filing_page = requests.get(url)
@@ -34,8 +38,10 @@ def find_xml(url):
     report_links = soup_2.find_all('a')
     for link in report_links:
         if link.text[-4:] == ('.txt'):
-            return(link.get('href'))
+            parse_xml(link.get('href'))
 
+
+#function to parse the report.
 def parse_xml(txt_link):
     #the XML
     
@@ -55,40 +61,74 @@ def parse_xml(txt_link):
     report_period = soup_3.find('periodOfReport')
     print(report_period.text)
 
+    #Get the CIK from the report, this is for the error catcher below.
+    report_cik = soup_3.find('cik')
+
+
     #parse the info tables
     
     info = soup_3.find_all('infoTable')
 
-    #create the TSV, name it after the organization and the period of the report.
-
-    csvFile = open(org_name.text + '_' + report_period.text +'.tsv', 'w', newline='')
-    csvWriter = csv.writer(csvFile, delimiter='\t', lineterminator='\n\n')
+    #create the TSV, name it after the organization and the period of the report. Try and except are for if there's an invalid name.
     
-    #set up the table
+    try:
+        csvFile = open(org_name.text + '_' + report_period.text +'.tsv', 'w', newline='')
+        csvWriter = csv.writer(csvFile, delimiter='\t', lineterminator='\n\n')
+        
+        #set up the table
 
-    csvWriter.writerow([org_name.text])
-    csvWriter.writerow([report_period.text])
-    csvWriter.writerow(' ')
-    csvWriter.writerow(['Name of Issuer', 'Sole Stock', 'Shared Stock', 'No Vote Stock', 'Total Value'])
+        csvWriter.writerow(['Name of Organization: ', org_name.text])
+        csvWriter.writerow(['Period of Report ', report_period.text])
+        csvWriter.writerow(' ')
+        csvWriter.writerow(['Name of Issuer', 'Sole Stock', 'Shared Stock', 'No Vote Stock', 'Total Value'])
 
-    for stock in info:
-        issuer = stock.find('nameOfIssuer')
-        value = stock.find('value')
-        sole_shares = stock.find('Sole')
-        shared_shares = stock.find('Shared')
-        novote_shares = stock.find('None')
-        csvWriter.writerow([issuer.text, sole_shares.text, shared_shares.text, novote_shares.text, value.text])
+        #looping through the info in the XML and printing it to the table
 
+        for stock in info:
+            issuer = stock.find('nameOfIssuer')
+            value = stock.find('value')
+            sole_shares = stock.find('Sole')
+            shared_shares = stock.find('Shared')
+            novote_shares = stock.find('None')
+            csvWriter.writerow([issuer.text, sole_shares.text, shared_shares.text, novote_shares.text, value.text])
+
+        csvFile.close()   
     
-    csvFile.close()    
+    except:
+          print('Hit an error with the report for CIK: ' + report_cik.text + ' for group ' + org_name.text + ' this is probably becauase of an invalid character in the name. Sorry.')
+    
+  
+
+
+# function to start the process and generate a list of CIKs. 
+
+def initiator():
+    starting_list = []
+    x = False
+    print("Hello! This is Rob Glass's CIK lookup. \n\n  ")
+    
+    new_CIK = ''
+
+    while new_CIK != 'Start' :
+        new_CIK = input('Please add a CIK, if you are finished adding CIKs input "Start": ')
+        if new_CIK == 'Start':
+          break
+        elif (len(new_CIK) != 10):
+            print('Sorry, that looks like an invalid CIK. Please recheck your CIK and try again.')
+        elif new_CIK.isdigit() == False:
+            print('Sorry, a CIK can only have numbers please try again.')
+        else:
+            starting_list.append(new_CIK)
+
+    for CIK in starting_list:
+        find_page(CIK)
+
+    print('All done!')
 
 
 
 
+initiator()
 
 
 
-
-
-
-parse_xml(find_xml(find_page(target)))
